fe2fdcea081aea2f5b3b63671051da2f18b13df3
amarpreetbhatia <amarpreetbhatia@gmail.com>
2020-08-09 23:23:39 +1000

    updated environment and iam role and vpc for understanding

--- aws-python-iam/handler.py
+++ aws-python-iam/handler.py
@@ -1,8 +1,12 @@
 import json
 import boto3
+import os
 
 
 def hello(event, context):
    client = boto3.client("lambda")
    response = client.list_functions()
+   print("-----------------------")
+   print(os.environ["FIRST_NAME"])
+   print("-----------------------")
    return response
